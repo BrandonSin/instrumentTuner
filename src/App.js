313f19018ce5e3e5b1a446bb17fd15a59b52e0a5
Brandon Sin <ancesz2000@yahoo.ca>
2021-03-03 09:52:00 -0700

    Added Note

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Tuner from './Tuner';
 import './App.css';
 
@@ -8,20 +7,7 @@ function App() {
     <div className="App">
       
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
         <Tuner />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
--- src/Tuner.js
+++ src/Tuner.js
@@ -5,16 +5,7 @@ import ReactSpeedometer from "react-d3-speedometer";
 import Wad from 'web-audio-daw';
 
 
-
 function Tuner(){
-    // let constraintObj = {
-    //     audio: true,
-    //     video: false,
-    // };
-    // const audio = document.getElementById('audio');
-    // function mic(){
-    //     navigator.mediaDevices.getUserMedia(constraintObj)
-    //     .then(function(mediaStream))
     
     const [frequencyValue, setFrequencyValue] = React.useState(440);
     const [states, setStates] = React.useState(0);
@@ -27,9 +18,7 @@ function Tuner(){
         var analyser = audioContext.createAnalyser();
         var voice = new Wad({source : 'mic' });
         var tune = new Wad.Poly();
-
-
-        
+  
     if (navigator.mediaDevices.getUserMedia) {
         console.log('getUserMedia supported.');
         var constraints = { audio: true }
@@ -37,7 +26,6 @@ function Tuner(){
             .then(function(stream) {
                 microphone = audioContext.createMediaStreamSource(stream);
                 microphone.connect(analyser);
-                //analyser.connect(audioContext.destination);
                 beginRecording();
             })
             .catch(function(err) {
@@ -48,11 +36,7 @@ function Tuner(){
     }
 
     function beginRecording() {
-
-        
-        
-
-        analyser.fftSize = 2048; // power of 2, between 32 and max unsigned integer
+        analyser.fftSize = 32768; // power of 2, between 32 and max unsigned integer
         var bufferLength = analyser.fftSize;
 
         var freqBinDataArray = new Uint8Array(bufferLength);
@@ -65,36 +49,20 @@ function Tuner(){
         var frequency =  ((index)*((audioContext.sampleRate/2)/ analyser.fftSize))
 
         var musicNote = calculateNote(frequency)
-        if (musicNote == undefined){
+        if (musicNote === undefined){
             setNote("...");
         }
         else{
             setNote(musicNote);
         }
-
-        // console.log('current Note: ' + musicNote);
-
         tune.add(voice);
         tune.updatePitch();
-        // console.log(tune.pitch);
-
+        
         var roundFrequency = frequency.toFixed(1);
         setFrequencyValue(roundFrequency);
-        // console.log('Freq Bin: ' + frequency);
-        //console.log(freqBinDataArray);
+        
         }
-
         setInterval(checkAudio, 64);
-    }
-
-    function getRMS(spectrum) {
-        var rms = 0;
-        for (var i = 0; i < spectrum.length; i++) {
-            rms += spectrum[i] * spectrum[i];
-        }
-        rms /= spectrum.length;
-        rms = Math.sqrt(rms);
-        return rms;
     }
 
     function getIndexOfMax(array) {
@@ -109,15 +77,12 @@ function Tuner(){
         return noteStrings[note%12]
     }
        
-
     }
     
-    
-
     return(
         <div className = "tuner">
             <h4>Violin Tuner</h4>
-            <div className = "gauge">
+            <p>Current Note: {currentNote}</p>
             <ReactSpeedometer
                 value={frequencyValue}
                 maxValue={880}
@@ -126,18 +91,10 @@ function Tuner(){
                 customSegmentStops={[0, 196, 293, 440, 659, 880]}
                 needleTransition ="easeElasticOut"
                 needleTransitionDuration={3000}
-                needleHeightRatio={0.7}
-                
-                 />
-            </div>
-            <p>Current Note: {currentNote}</p>
-            
-            
-            <button onClick={vTuner} className = "start" style={{visibility: states == 0 ? 'visible' : 'hidden' }}>Start</button>
-        </div>
-        
-    )
-    
+                needleHeightRatio={0.7}    
+                 />    
+            <button onClick={vTuner} className = "start" style={{visibility: states === 0 ? 'visible' : 'hidden' }}>Start</button>
+        </div>   
+    ) 
 }
-
 export default Tuner;
