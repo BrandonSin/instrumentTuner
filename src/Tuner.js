001069d1e45776b3f7696d58ad1c3ec73cf71d2f
Brandon Sin <ancesz2000@yahoo.ca>
2021-03-03 12:56:46 -0700

    Added Note

--- src/Tuner.js
+++ src/Tuner.js
@@ -2,7 +2,6 @@ import './App.css';
 import './index.css';
 import * as React from "react";
 import ReactSpeedometer from "react-d3-speedometer";
-import Wad from 'web-audio-daw';
 
 
 function Tuner(){
@@ -10,16 +9,18 @@ function Tuner(){
     const [frequencyValue, setFrequencyValue] = React.useState(440);
     const [states, setStates] = React.useState(0);
     const [currentNote, setNote] = React.useState("A");
-    var AudioContext = window.AudioContext || window.webkitAudioContext;
-    var audioContext = new AudioContext();  
+    const [audioContext, setAudioCtx] = React.useState(0);
+    const [analyser, setAnalyser] = React.useState(0);
+
+
+    
 
     function vTuner(){
-        audioContext.resume()
+        var audioContext = new(window.AudioContext || window.webkitAudioContext)();
         var microphone;
         setStates(1);
         var analyser = audioContext.createAnalyser();
-        var voice = new Wad({source : 'mic' });
-        var tune = new Wad.Poly();
+       
   
     if (navigator.mediaDevices.getUserMedia) {
         console.log('getUserMedia supported.');
@@ -40,25 +41,19 @@ function Tuner(){
     function beginRecording() {
         analyser.fftSize = 32768; // power of 2, between 32 and max unsigned integer
         var bufferLength = analyser.fftSize;
-
         var freqBinDataArray = new Uint8Array(bufferLength);
 
         var checkAudio = function() {
         analyser.getByteFrequencyData(freqBinDataArray);
-        
-
         var index = getIndexOfMax(freqBinDataArray)
         var frequency =  ((index)*((audioContext.sampleRate/2)/ analyser.fftSize))
-
-        var musicNote = calculateNote(frequency)
+        var musicNote = findNote(frequency)
         if (musicNote === undefined){
             setNote("...");
         }
         else{
             setNote(musicNote);
         }
-        tune.add(voice);
-        tune.updatePitch();
         
         var roundFrequency = frequency.toFixed(1);
         setFrequencyValue(roundFrequency);
@@ -71,16 +66,22 @@ function Tuner(){
         return array.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
     }
 
-    function calculateNote(frequency){
+    function findNote(frequency){
         // https://en.wikipedia.org/wiki/Pitch_(music) Formula for Pitches
         var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];  
         var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
         var note = Math.round( noteNum ) + 69;
         return noteStrings[note%12]
     }
+
+   
        
     }
-    
+    React.useEffect( ()=>{
+        if(audioContext !== 0){
+          setAnalyser(audioContext.createAnalyser());
+        }    
+      },[audioContext])
     return(
         <div className = "tuner">
             <h4>Violin Tuner</h4>
@@ -92,7 +93,6 @@ function Tuner(){
                 minValue={0}
                 currentValueText={'Frequency: ${value}'}
                 customSegmentStops={[0, 196, 293, 440, 659, 880]}
-                needleTransition ="easeElasticOut"
                 needleTransitionDuration={3000}
                 needleHeightRatio={0.7}    
                  />    
